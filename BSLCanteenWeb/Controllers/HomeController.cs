516514c29b51af0ab914249843490f077a72ebbd
Kanchan <banswaraws@gmail.com>
2026-07-06 11:54:08 +0530

    Modified in HomeController.cs new controller function added i.e. Fn_Login_Employee() and merge - Kanchan

--- BSLCanteenWeb/Controllers/HomeController.cs
+++ BSLCanteenWeb/Controllers/HomeController.cs
@@ -38,6 +38,32 @@ namespace BSLCanteenWeb.Controllers
             return View();
         }
 
+
+        [HttpPost]
+        public JsonResult Fn_Login_Employee(clsEmployee objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLCanteenAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/Employee/Fn_Login_Employee", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Login Failed" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+
         [HttpPost]
         public JsonResult Fn_Fill_DropdownList(clsRequestDropdown cs)
         {
