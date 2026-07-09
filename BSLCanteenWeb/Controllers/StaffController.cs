649d2e75d583b0c8fc7d9a072186755f5e166585
Kanchan <banswaraws@gmail.com>
2026-07-09 16:09:54 +0530

    Modified in StaffController.cs added the new controller function Fn_Get_Coupon_Order() - Kanchan

--- BSLCanteenWeb/Controllers/StaffController.cs
+++ BSLCanteenWeb/Controllers/StaffController.cs
@@ -41,5 +41,32 @@ namespace BSLCanteenWeb.Controllers
                 }
             }
         }
+
+
+        [HttpPost]
+        public JsonResult Fn_Get_Coupon_Order(clsCouponReport objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLCANTEENAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/Canteen/Fn_Get_Coupon_Order", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Scanned Coupons List fetching failed." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+
     }
 }
