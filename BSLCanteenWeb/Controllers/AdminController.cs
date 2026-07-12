0170be80b42f89f072e4651d5cacd426f1649ff2
Ankit <banswaraws@gmail.com>
2026-07-12 16:28:36 +0530

    Created Fn_Monthly_Report API calling function and Report page content and layout and added new clas for report

--- BSLCanteenWeb/Controllers/AdminController.cs
+++ BSLCanteenWeb/Controllers/AdminController.cs
@@ -281,6 +281,28 @@ namespace BSLCanteenWeb.Controllers
             }
         }
 
+        [HttpPost]
+        public JsonResult Fn_Monthly_Report(clsMonthlyReportReq cs)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLCANTEENAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(cs);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/Canteen/Fn_Monthly_Report", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "No data found." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
     }
 }
