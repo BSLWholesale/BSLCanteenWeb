5a488cda84360703795eb67bc7ca43d087fa1de0
Ankit <banswaraws@gmail.com>
2026-07-21 17:15:37 +0530

    Created ManualCouponEntry apge content and layout and header page and updared Staff dashboard page

--- BSLCanteenWeb/Controllers/StaffController.cs
+++ BSLCanteenWeb/Controllers/StaffController.cs
@@ -21,6 +21,10 @@ namespace BSLCanteenWeb.Controllers
         {
             return View();
         }
+        public ActionResult ManualCouponEntry()
+        {
+            return View();
+        }
 
         [HttpPost]
         public JsonResult Fn_ProcessCouponTransaction(clsCouponReport objReq)
@@ -71,6 +75,29 @@ namespace BSLCanteenWeb.Controllers
             }
         }
 
+        [HttpPost]
+        public JsonResult Fn_Cancel_CouponId(clsCouponReport objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLCANTEENAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/Canteen/Fn_Cancel_CouponId", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Cancel_CouponId failed." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
 
     }
 }
