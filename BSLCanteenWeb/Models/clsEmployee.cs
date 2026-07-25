43974a6f64539d1cd82e3f161d719d62eb027022
Kanchan <banswaraws@gmail.com>
2026-07-25 19:21:26 +0530

    Added the new class clsCategoryReport in in clsEmployee.cs - Kanchan

--- BSLCanteenWeb/Models/clsEmployee.cs
+++ BSLCanteenWeb/Models/clsEmployee.cs
@@ -161,4 +161,30 @@ namespace BSLCanteenWeb.Models
         public string vErrorMsg { get; set; }
         public int vErrorCode { get; set; }
     }
+
+
+    public class clsCategoryReport
+    {
+        public int CanteenId { get; set; }
+        public string CanteenName { get; set; }
+        public string CurrentDate { get; set; }
+        public Int32 EmpId { get; set; }
+        public string Noofworkers { get; set; }
+        public string GeneratedCoupons { get; set; }
+        public string ScannedCoupons { get; set; }
+        public string GeneratedCouponTea { get; set; }
+        public string QRScannedTea { get; set; }
+        public string GeneratedCouponBreakfast { get; set; }
+        public string QRScannedBreakfast { get; set; }
+        public string GeneratedCouponThali { get; set; }
+        public string QRScannedThali { get; set; }
+        public string GeneratedCouponMiniThali { get; set; }
+        public string QRScannedMiniThali { get; set; }
+        public string ItemCategory { get; set; }
+        public string ReportType { get; set; }
+        public string EmpLocation { get; set; }
+        public string vErrorMsg { get; set; }
+    }
+
+
 }
