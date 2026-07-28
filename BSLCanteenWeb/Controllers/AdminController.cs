ded873a4dc368704259538f1cb2a7258aa9fdd69
Kanchan <banswaraws@gmail.com>
2026-07-28 17:27:34 +0530

    Added the new controller function Fn_DailyReport_Shiftwise() in AdminController.cs - Kanchan

--- BSLCanteenWeb/Controllers/AdminController.cs
+++ BSLCanteenWeb/Controllers/AdminController.cs
@@ -71,6 +71,7 @@ namespace BSLCanteenWeb.Controllers
             return View();
         }
 
+
         [HttpPost]
         public JsonResult Fn_Fetch_EmployeeDetails(clsEmployee objReq)
         {
@@ -580,6 +581,30 @@ namespace BSLCanteenWeb.Controllers
             }
         }
 
+
+        [HttpPost]
+        public JsonResult Fn_DailyReport_Shiftwise(clsShiftReport objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLCANTEENAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/Canteen/Fn_DailyReport_Shiftwise", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Daily Shift wise records are not found." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
 
 
     }
