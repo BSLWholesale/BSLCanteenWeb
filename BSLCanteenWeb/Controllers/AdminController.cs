83e7ed032ec72b362ec22bdd868681d81882ddc4
Kanchan <banswaraws@gmail.com>
2026-07-29 15:29:56 +0530

    Remove the un-necessary code from AdminController.cs - Kanchan

--- BSLCanteenWeb/Controllers/AdminController.cs
+++ BSLCanteenWeb/Controllers/AdminController.cs
@@ -66,11 +66,6 @@ namespace BSLCanteenWeb.Controllers
             return View();
         }
 
-        public ActionResult DailyCategorywiseReport()
-        {
-            return View();
-        }
-
 
         [HttpPost]
         public JsonResult Fn_Fetch_EmployeeDetails(clsEmployee objReq)
@@ -528,80 +523,6 @@ namespace BSLCanteenWeb.Controllers
                 else
                 {
                     return Json(new { success = false, message = "Employee Password reset failed." }, JsonRequestBehavior.AllowGet);
-                }
-            }
-        }
-
-
-        [HttpPost]
-        public JsonResult Fn_DailyReport_ItemCategorywise(clsCategoryReport objReq)
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLCANTEENAPIURL"]));
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
-
-                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
-                HttpResponseMessage responsePost = client.PostAsync("api/Canteen/Fn_DailyReport_ItemCategorywise", content).Result;
-                if (responsePost.IsSuccessStatusCode)
-                {
-                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
-                }
-                else
-                {
-                    return Json(new { success = false, message = "Daily category wise report records are not found." }, JsonRequestBehavior.AllowGet);
-                }
-            }
-        }
-
-        [HttpPost]
-        public JsonResult Fn_DailyReport_Canteenwise(clsCategoryReport objReq)
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLCANTEENAPIURL"]));
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
-
-                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
-                HttpResponseMessage responsePost = client.PostAsync("api/Canteen/Fn_DailyReport_Canteenwise", content).Result;
-                if (responsePost.IsSuccessStatusCode)
-                {
-                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
-                }
-                else
-                {
-                    return Json(new { success = false, message = "Daily Canteen wise records are not found." }, JsonRequestBehavior.AllowGet);
-                }
-            }
-        }
-
-
-        [HttpPost]
-        public JsonResult Fn_DailyReport_Shiftwise(clsShiftReport objReq)
-        {
-            using (var client = new HttpClient())
-            {
-                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLCANTEENAPIURL"]));
-                client.DefaultRequestHeaders.Accept.Clear();
-                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
-
-                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
-                HttpResponseMessage responsePost = client.PostAsync("api/Canteen/Fn_DailyReport_Shiftwise", content).Result;
-                if (responsePost.IsSuccessStatusCode)
-                {
-                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
-                }
-                else
-                {
-                    return Json(new { success = false, message = "Daily Shift wise records are not found." }, JsonRequestBehavior.AllowGet);
                 }
             }
         }
