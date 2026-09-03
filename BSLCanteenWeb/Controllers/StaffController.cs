69cc185a5fc9ca081df88f570171e303a0d158bb
Kanchan <banswaraws@gmail.com>
2026-09-03 17:36:14 +0530

    Added the new controller function i.e. Fn_DailyReport_CouponTypeWise() in CanteenController.cs - Kanchan

--- BSLCanteenWeb/Controllers/StaffController.cs
+++ BSLCanteenWeb/Controllers/StaffController.cs
@@ -369,6 +369,31 @@ namespace BSLCanteenWeb.Controllers
         }
 
 
+        [HttpPost]
+        public JsonResult Fn_DailyReport_CouponTypeWise(clsCouponTypeReport objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLCANTEENAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/Canteen/Fn_DailyReport_CouponTypeWise", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Daily Coupon Type wise records are not found." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+
 
     }
 }
