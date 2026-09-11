e2733b078eaedd043ff8182ec34bc5e948d0b845
Kanchan <banswaraws@gmail.com>
2026-09-11 11:06:38 +0530

    Modified in StaffController.cs added the controller function Fn_Upload_UPIDataFile(), Fn_DailyReport_UPIDataCanteenWise() and Fn_MonthlyReport_UPIDataCanteenWise() - Kanchan

--- BSLCanteenWeb/Controllers/StaffController.cs
+++ BSLCanteenWeb/Controllers/StaffController.cs
@@ -7,6 +7,10 @@ using System.Net.Http;
 using System.Text;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
+using OfficeOpenXml;
+using System.IO;
+using System.Globalization;
 
 namespace BSLCanteenWeb.Controllers
 {
@@ -45,6 +49,17 @@ namespace BSLCanteenWeb.Controllers
         {
             return View();
         }
+
+        public ActionResult UPIDataUpload()
+        {
+            return View();
+        }
+
+        public ActionResult UPIMonthlyDataReport()
+        {
+            return View();
+        }
+
 
         [HttpPost]
         public JsonResult Fn_ProcessCouponTransaction(clsCouponReport objReq)
@@ -393,6 +408,131 @@ namespace BSLCanteenWeb.Controllers
             }
         }
 
+
+        [HttpPost]
+        public JsonResult Fn_Upload_UPIDataFile(clsUPIDataUpload objReq)
+        {
+            objReq.vErrorMsg = "";
+            HttpPostedFileBase file = Request.Files[0];
+
+            if (file == null || file.ContentLength == 0)
+            {
+                return Json(new { status = "error", message = "Please Select Excel file" });
+            }
+
+            string fileExtension = Path.GetExtension(file.FileName).ToLower();
+
+            if (fileExtension != ".xls" && fileExtension != ".xlsx")
+            {
+                return Json(new { success = false, message = "Invalid file format. Only .xls or .xlsx are allowed." }, JsonRequestBehavior.AllowGet);
+            }
+
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            List<clsUPIDataUpload> UPIDataList = new List<clsUPIDataUpload>();
+
+            using (ExcelPackage package = new ExcelPackage(file.InputStream))
+            {
+                ExcelWorksheet sheet = package.Workbook.Worksheets[0];
+                int rowCount = sheet.Dimension.End.Row;
+
+                for (int row = 2; row <= rowCount; row++)
+                {
+                    string Transdate;
+
+                    if (sheet.Cells[row, 1].Value is DateTime)
+                    {
+                        Transdate = ((DateTime)sheet.Cells[row, 1].Value).ToString("dd-MM-yyyy HH:mm:ss");
+                    }
+                    else
+                    {
+                        Transdate = DateTime.ParseExact(sheet.Cells[row, 1].Text, "dd-MM-yyyy HH:mm:ss", CultureInfo.InvariantCulture).ToString("dd-MM-yyyy HH:mm:ss");
+                    }
+
+                    clsUPIDataUpload UPIData = new clsUPIDataUpload
+                    {
+                        TransactionDate = Transdate,
+                        TransactionID = Convert.ToString(sheet.Cells[row, 2].Value),
+                        Amount = Convert.ToDecimal(sheet.Cells[row, 3].Value),
+                        Rate = Convert.ToDecimal(sheet.Cells[row, 4].Value),
+                        Qty = Convert.ToInt32(sheet.Cells[row, 5].Value),
+                        LocationID = Convert.ToString(sheet.Cells[row, 6].Value)
+                    };
+
+                    UPIDataList.Add(UPIData);
+                }
+            }
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(ConfigurationManager.AppSettings["BSLCANTEENAPIURL"]);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(UPIDataList);
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+
+                HttpResponseMessage responsePost = client.PostAsync("api/Canteen/Fn_Upload_UPIDataFile", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "UPI Data Import Failed" }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+
+        [HttpPost]
+        public JsonResult Fn_DailyReport_UPIDataCanteenWise(clsUPIDataReport objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLCANTEENAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/Canteen/Fn_DailyReport_UPIDataCanteenWise", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "Daily UPI data report records are not found." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
+
+
+        [HttpPost]
+        public JsonResult Fn_MonthlyReport_UPIDataCanteenWise(clsUPIMonthlyReportRequest objReq)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Convert.ToString(ConfigurationManager.AppSettings["BSLCANTEENAPIURL"]));
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                string DATA = Newtonsoft.Json.JsonConvert.SerializeObject(objReq);
+
+                HttpContent content = new StringContent(DATA, UTF8Encoding.UTF8, "application/json");
+                HttpResponseMessage responsePost = client.PostAsync("api/Canteen/Fn_MonthlyReport_UPIDataCanteenWise", content).Result;
+                if (responsePost.IsSuccessStatusCode)
+                {
+                    return Json(new { success = true, message = responsePost.Content.ReadAsStringAsync().Result }, JsonRequestBehavior.AllowGet);
+                }
+                else
+                {
+                    return Json(new { success = false, message = "UPI Monthly Data records are not found." }, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
 
 
     }
